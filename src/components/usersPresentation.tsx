c524adcf5fad92f894d23902d78eb65b44cca48e
Mateusz Taraszka <m.taraszka98@gmail.com>
2022-09-16 16:44:06 +0200

    add table head, styles changes

--- src/components/usersPresentation.tsx
+++ src/components/usersPresentation.tsx
@@ -5,6 +5,13 @@ import {UserPresentedData} from "../types";
 export const UsersPresentation: FC<{ presentationData: UserPresentedData[] }> = ({presentationData}) => {
     return (
         <table className={'table-styles'}>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Profit & Loss</th>
+                    <th>Account Type</th>
+                </tr>
+            </thead>
             {presentationData.map(({id, name, profitAndLoss, accountType}) => (
                 <tr key={id}>
                     <td>
